5e7e6e7bba425a5d9a39edcde109b01539019fc5
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-21 21:29:42 +0545

    Repository: UserRepo

---
+++ Spring-Project-employee/src/main/java/com/springproject/employee/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.springproject.employee.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springproject.employee.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+	User findByUsernameAndPassword(String un, String psw);
+	
+	User findByUsername(String un);
+}
