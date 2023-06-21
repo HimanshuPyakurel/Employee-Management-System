d0cf862788e0b647f8857d680284eb069f19bb0f
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-21 21:30:25 +0545

    SpringProjectEmployeeApplication.java

---
+++ Spring-Project-employee/src/main/java/com/springproject/employee/SpringProjectEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectEmployeeApplication.class, args);
+	}
+
+}
