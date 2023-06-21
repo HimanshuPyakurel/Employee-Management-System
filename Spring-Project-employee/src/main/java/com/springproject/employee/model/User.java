a1de2867754386942347493d02aa2e49ee1a431e
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-21 21:10:48 +0545

    model: User

---
+++ Spring-Project-employee/src/main/java/com/springproject/employee/model/User.java
@@ -0,0 +1,27 @@
+package com.springproject.employee.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "user_tbl")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String fname;
+	private String lname;
+	
+	@Column(unique = true)
+	private String username;
+	private String password;
+	private String email;
+	
+}
