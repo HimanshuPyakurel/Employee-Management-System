bf5a2c4cde5e2db684f3f92d514fa99689361bd2
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-21 21:28:46 +0545

    add: User Service Impl class

---
+++ Spring-Project-employee/src/main/java/com/springproject/employee/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.springproject.employee.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springproject.employee.model.User;
+import com.springproject.employee.repository.UserRepository;
+import com.springproject.employee.service.IUserService;
+
+@Service
+public class UserServiceImpl implements IUserService{
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Override
+	public void signup(User user) {
+		userRepo.save(user);
+	}
+
+	@Override
+	public User login(String un, String psw) {
+		
+		return userRepo.findByUsernameAndPassword(un, psw);
+	}
+
+	@Override
+	public User isUserExist(String un) {
+		
+		return userRepo.findByUsername(un);
+	}
+
+}
