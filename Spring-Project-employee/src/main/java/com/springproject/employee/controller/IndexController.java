60a2baa4a133af9489c2fc8ed16a4215b8c56345
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-22 23:22:43 +0545

    controller: IndexController.java

---
+++ Spring-Project-employee/src/main/java/com/springproject/employee/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.springproject.employee.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/")
+	public String getIndex() {
+		
+		return "LoginForm";
+	}
+
+}
