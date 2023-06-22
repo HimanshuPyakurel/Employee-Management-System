469ebf1159f0a0fefc4387fab0ea5fee6c0c123e
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-22 23:10:00 +0545

    add: UserRepository.java

--- Spring-Project-employee/src/main/java/com/springproject/employee/repository/UserRepository.java
+++ Spring-Project-employee/src/main/java/com/springproject/employee/repository/UserRepository.java
@@ -4,9 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springproject.employee.model.User;
 
-public interface UserRepository extends JpaRepository<User, Integer>{
-	
-	User findByUsernameAndPassword(String un, String psw);
+public interface UserRepository  extends JpaRepository<User, Integer>{
+
+	User  findByUsernameAndPassword(String un, String psw);
 	
 	User findByUsername(String un);
 }
