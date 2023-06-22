baf5bcce48f656f10c503a5869434a490de3a389
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-22 23:22:16 +0545

    controller: UserController

--- Spring-Project-employee/src/main/java/com/springproject/employee/controller/UserController.java
+++ Spring-Project-employee/src/main/java/com/springproject/employee/controller/UserController.java
@@ -2,6 +2,8 @@ package com.springproject.employee.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,29 +12,51 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.springproject.employee.model.User;
 import com.springproject.employee.service.IUserService;
 
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	
-	@Autowired
-	private IUserService userservice;
-	
-	@GetMapping("/login")
-	public String getLogin() {
-		return "LoginForm";
-	}
-	
-	@PostMapping("/login")
-	public String postLogin(@ModelAttribute User user) {
+
+		@Autowired
+		private IUserService  userService;
 		
-		User usr= userservice.login(user.getUsername(), user.getPassword());
-		
-		if(usr != null) {
-			return "Home";
+		@GetMapping("/login")
+		public String getLogin() {
+			
+			return "LoginForm";
 		}
-		return "LoginForm";
+		
+		@PostMapping("/login")
+		public String postLogin(@ModelAttribute User user, Model model) {
+			
+			user.setPassword(DigestUtils.md5DigestAsHex(user.getPassword().getBytes()));
+			User  usr = userService.login(user.getUsername(), user.getPassword());
+			
+			if(usr != null) {
+				
+				model.addAttribute("user",usr);
+				
+				return "Home";
+			}
+			model.addAttribute("message","user not found!!");
+			return  "LoginForm";
+		}
+		
+		
+		@GetMapping("/signup")
+		public String getSignup() {
+			
+			return "SignupForm";
+		}
+		
+		@PostMapping("/signup")
+		public String postSignup(@ModelAttribute User user) {
+			
+			user.setPassword(DigestUtils.md5DigestAsHex(user.getPassword().getBytes()));
+			userService.signup(user);
+			
+			return "LoginForm";
+		}	
+		
+		
 	}
-	
-	
-	
-}
