ad2af4da85d2323fb4df453db135e225d82a3067
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-23 22:18:10 +0545

    feat: Department Controller with method to list, delete and edit

--- Spring-Project-employee/src/main/java/com/springproject/employee/controller/DepartmentController.java
+++ Spring-Project-employee/src/main/java/com/springproject/employee/controller/DepartmentController.java
@@ -2,10 +2,12 @@ package com.springproject.employee.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.springproject.employee.model.Department;
 import com.springproject.employee.service.DepartmentService;
@@ -26,8 +28,42 @@ public class DepartmentController {
 	  @PostMapping("/add")
 	 public String postDepartment(@ModelAttribute Department  dept) {
 		  
-		  deptService.addDept(dept);
+		 deptService.addDept(dept);
 		  
 		 return "DepartmentForm";
 	 }
+	  
+	 @GetMapping("/list")
+	 public String deptlist(Model model) {
+		 
+		 model.addAttribute("deptList",deptService.getAllDepts());
+		 
+		 return "DepartmentListForm";
+	 }
+	 
+	 @GetMapping("/delete")
+	 public String delete(@RequestParam int id) {
+		 	
+		 	deptService.deleteDept(id);
+		 
+		 return "redirect:/department/list";
+	 }
+	 
+	 @GetMapping("/edit")
+	 public String edit(@RequestParam int id, Model model) {
+		 	
+		 model.addAttribute("deptObject",deptService.getDeptById(id));
+		 
+		 return "DepartmentEditForm";
+	 }
+	 
+	 @PostMapping("/update")
+	 public String update(@ModelAttribute Department dept) {
+		 
+		 deptService.updateDept(dept);
+		 
+		 return "redirect:/department/list"; 
+	 }
+	 
 }
+
