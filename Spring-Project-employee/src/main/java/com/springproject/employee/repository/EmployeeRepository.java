abdfcc622139fb24bba2e880ce1cdfc8e82becde
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-24 19:09:50 +0545

    repository: Employee

---
+++ Spring-Project-employee/src/main/java/com/springproject/employee/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.springproject.employee.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springproject.employee.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+
+}
