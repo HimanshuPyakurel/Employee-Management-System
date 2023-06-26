e994c41e52714909a79983fd03fd23758295151e
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-26 22:00:37 +0545

    model: Employee

--- Spring-Project-employee/src/main/java/com/springproject/employee/model/Employee.java
+++ Spring-Project-employee/src/main/java/com/springproject/employee/model/Employee.java
@@ -1,5 +1,11 @@
 package com.springproject.employee.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,20 +13,45 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "employee_tbl")
+@Table(name ="employee_tbl")
 public class Employee {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	private String name;
+	private Long id;
+	private String fname;
+	private String lname;
+	private String gender;
+	private String email;
 	private String phone;
-	private String address;
+	private String company;
+	private String post;
+	private int salary;
+	
+	@DateTimeFormat(iso = ISO.DATE)
+	private LocalDate dob;
+	
+	@DateTimeFormat(iso = ISO.DATE)
+	private LocalDate joinDate;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="addressId")
+	private Address address;
 	
 	@OneToOne
 	@JoinColumn(name="departmentId")
 	private Department department;
+	
+	@ElementCollection
+	@CollectionTable
+	private List<String> projects;
+	
 }
