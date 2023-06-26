5dd40d6dc5ce80c67785e3c26179a7d6e799999d
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-26 22:04:26 +0545

    feat: Id datatype change to Long

--- Spring-Project-employee/src/main/java/com/springproject/employee/repository/EmployeeRepository.java
+++ Spring-Project-employee/src/main/java/com/springproject/employee/repository/EmployeeRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springproject.employee.model.Employee;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 
 }
