a8eb3f57c4a7efc883289844ee00e40bd7d3e851
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-28 22:09:46 +0545

    controller: UploadController

---
+++ Spring-Project-employee/src/main/java/com/springproject/employee/controller/UploadController.java
@@ -0,0 +1,37 @@
+package com.springproject.employee.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.springproject.employee.utils.FileUtil;
+
+@Controller
+public class UploadController {
+	
+	@Autowired
+	private FileUtil fileUtil;
+	
+	@GetMapping("/upload")
+	public String getUpload() {
+		return "UploadForm";
+	}
+	
+	@PostMapping("/upload")
+	public String postUpload(@RequestParam MultipartFile image, Model model) {
+		
+		if(!image.isEmpty()) {
+			
+		fileUtil.fileUpload(image);
+		model.addAttribute("message","Upload Success");
+		return "UploadForm";
+		}
+		
+		model.addAttribute("message","Upload Failed");
+		return "UploadForm";
+	}
+}
