c4505312737016eefa7bbcd73313c372d37d7953
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-06-30 21:01:17 +0545

    feat: Http Session

--- Spring-Project-employee/src/main/java/com/springproject/employee/controller/GalleryController.java
+++ Spring-Project-employee/src/main/java/com/springproject/employee/controller/GalleryController.java
@@ -2,6 +2,8 @@ package com.springproject.employee.controller;
 
 import java.io.File;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class GalleryController {
 	
 	@GetMapping("/gallery")
-	public String getGallery(Model model) {
+	public String getGallery(Model model, HttpSession session) {
+		
+		if(session.getAttribute("validuser") == null) {
+			
+			return "LoginForm";
+		}
 		
 		String[] imageNames = new File("src/main/resources/static/images").list();
 		model.addAttribute("imgNameList",imageNames);
