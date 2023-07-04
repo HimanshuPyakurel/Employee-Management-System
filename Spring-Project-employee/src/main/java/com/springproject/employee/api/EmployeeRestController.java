cbb90ff2a9133b1cabb2a91f29d542a7bb524851
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-04 22:22:31 +0545

    feat: RestApi for particular id

--- Spring-Project-employee/src/main/java/com/springproject/employee/api/EmployeeRestController.java
+++ Spring-Project-employee/src/main/java/com/springproject/employee/api/EmployeeRestController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springproject.employee.model.Employee;
@@ -20,6 +22,11 @@ public class EmployeeRestController {
 		
 		return empService.getallEmp();
 		
+	}
+	
+	@PostMapping("/api/emp/{id}")
+	public Employee getOneEmp(@PathVariable Long id) {
+		return empService.getEmpById(id);
 	}
 	
 }
