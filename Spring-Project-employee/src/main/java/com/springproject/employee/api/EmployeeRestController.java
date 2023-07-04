61237cce44234ace83cceb3fe8dac7b9f411b4a2
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-04 22:18:24 +0545

    controller: RestApi for Employee List

---
+++ Spring-Project-employee/src/main/java/com/springproject/employee/api/EmployeeRestController.java
@@ -0,0 +1,25 @@
+package com.springproject.employee.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springproject.employee.model.Employee;
+import com.springproject.employee.service.EmployeeService;
+
+@RestController
+public class EmployeeRestController {
+	
+	@Autowired
+	private EmployeeService empService;
+	
+	@GetMapping("/api/emp/list")
+	public List<Employee> getAll() {
+		
+		return empService.getallEmp();
+		
+	}
+	
+}
