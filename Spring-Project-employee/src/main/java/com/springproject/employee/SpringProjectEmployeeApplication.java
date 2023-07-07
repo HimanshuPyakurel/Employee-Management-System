ed1627383b875f6de43f27c5eed585c2de9ce14a
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-07 21:18:34 +0545

    feat: Open API Definition

--- Spring-Project-employee/src/main/java/com/springproject/employee/SpringProjectEmployeeApplication.java
+++ Spring-Project-employee/src/main/java/com/springproject/employee/SpringProjectEmployeeApplication.java
@@ -3,7 +3,11 @@ package com.springproject.employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Employee Rest API", version = "4.0", description = "employee CRUD apis"))
 public class SpringProjectEmployeeApplication {
 
 	public static void main(String[] args) {
