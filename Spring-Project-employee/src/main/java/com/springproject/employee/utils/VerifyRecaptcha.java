9f64eb7370274faed3a918ca9dbba7cb4a548f39
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-07-08 09:27:38 +0545

    utils: Verify recaptcha

--- Spring-Project-employee/src/main/java/com/springproject/employee/utils/VerifyRecaptcha.java
+++ Spring-Project-employee/src/main/java/com/springproject/employee/utils/VerifyRecaptcha.java
@@ -15,7 +15,7 @@ import javax.net.ssl.HttpsURLConnection;
 public class VerifyRecaptcha {
 
 	public static final String url = "https://www.google.com/recaptcha/api/siteverify";
-	public static final String secret = "6LcibvwmAAAAAMlv-CP09EpaCOtSMRj8nf2he36V";
+	public static final String secret = "";
 	private final static String USER_AGENT = "Mozilla/5.0";
 
 	public static boolean verify(String gRecaptchaResponse) throws IOException {
